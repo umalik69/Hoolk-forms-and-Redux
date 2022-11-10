d59b011721727e318208cd789aff05aff55a8682
umalik69 <sheerdil79@gmail.com>
2022-11-10 21:46:19 +0500

    changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -17,23 +17,43 @@ export default function Form() {
     }
    
   return (
-    <div>
+    <div >
     <form onSubmit={handleSubmit(onSubmit)}>
-    <select {...register("Name")} className="select">
-        <option value="Hussain">Hussain</option>
-        <option value="Usama">Usama</option>
-        <option value="Ahmad">Ahmad</option>
+    <div className='form'>
+    <input 
+    className='select'
+        {...register("Name", { required: true })} 
+        aria-invalid={errors.Name ? "true" : "false"} 
+      />
+      {errors.Name?.type === 'required' && <p role="alert">First name is required</p>}
+
+      
+    <select    {...register("mail", { required: "Email Address is required" })} 
+        aria-invalid={errors.mail ? "true" : "false"}  className="select">
+        <option value="20">20</option>
+        <option value="30">30</option>
+        <option value="50">50</option>
+      </select>
+      <select {...register("Gender")} className="select">
+        <option value="Female">Female</option>
+        <option value="Male">Male</option>
+
       </select>
 
     {errors.Name?.type === 'required' && <p role="alert">First name is required</p>}
       <input type="submit" className='submit' />
+      </div>
     </form>
-    <div>
+    <div className='maintable'>
         {
         list.map((elem)=>{
             return(
-            <div key={elem.id}>
-    <h3>{elem.data.Name} < AiFillCloseCircle onClick={()=>dispatch(DLT(elem.id))}/></h3>
+            <div key={elem.id} className="table">
+    <h3>{elem.data.Name} </h3>
+    <h3>{elem.data.mail} </h3>
+    <h3>{elem.data.Gender} </h3>
+
+    <button onClick={()=>dispatch(DLT(elem.id))}>Delete</button>
     </div>
          ) })
 }
